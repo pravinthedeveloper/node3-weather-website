f4a2f277cec1b7769d6ddde1a32b068b95183c34
Pravin Kunhiraman <pravin.kunhiraman@gmail.com>
2021-10-03 08:02:33 +1100

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript file is loaded!')
 // Use Fetch API to get weather JSON 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
